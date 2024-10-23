2897fb5a677342ce076ef0ea69204b37a7cfe07a
Askadhhh <127306364+Askadhhh@users.noreply.github.com>
2024-10-23 21:50:51 +0500

    добавили уникальный айди вопроса
    Co-authored-by: qvazi <qvazi@users.noreply.github.com>

--- tasks/211024-KnowledgeQuizzes/createQuestions/script.js
+++ tasks/211024-KnowledgeQuizzes/createQuestions/script.js
@@ -2,6 +2,8 @@ const addAnswerForm = document.querySelector("#addAnswerForm");
 const containerAnswer = document.querySelector("#containerAnswer");
 const createQuestionForm = document.querySelector("#createQuestionForm");
 
+addAnswer();
+
 // Добавление нового варианта ответа
 addAnswerForm.addEventListener("submit", (event) => {
   event.preventDefault();
@@ -10,16 +12,7 @@ addAnswerForm.addEventListener("submit", (event) => {
 
   const text = formData.get("newTextAnswer").trim();
 
-  containerAnswer.innerHTML += `<div>
-          <input type="radio" name="currectAnswer" required />
-          <input
-            type="text"
-            name="answers"
-            value="${text}"
-            required
-          />
-          <input class="deleteButton" type="button" value="Удалить" />
-        </div>`;
+  addAnswer(text);
 
   event.target.reset();
 });
@@ -38,15 +31,54 @@ containerAnswer.addEventListener("click", (event) => {
 createQuestionForm.addEventListener("submit", (event) => {
   event.preventDefault();
 
+  const answersCount = containerAnswer.querySelectorAll("div").length;
+
+  if (answersCount < 2) {
+    alert("Должно быть минимум два варианта ответа!");
+    return;
+  }
+
   const formData = new FormData(event.target);
 
-  const newQuestion = {
-    title: formData.get("title"),
-    answers: formData.getAll("answers"),
-    correctAnswer: [
-      ...containerAnswer.querySelectorAll('input[type="radio"]'),
-    ].findIndex((radio) => radio.checked),
-  };
+  const answersIds = formData.getAll("answersIds");
+  const answers = formData.getAll("answers");
 
-  console.log(newQuestion);
+  const newQuestion = {
+    id: getId(),
+    title: formData.get("title"),
+    answers: answers.map((answer, index) => {
+      return {
+        id: answersIds[index],
+        text: answer,
+      };
+    }),
+    correctAnswer: formData.get("correctAnswer"),
+  };
 });
+
+function addAnswer(text = "") {
+  const id = getId();
+  containerAnswer.innerHTML += `<div>
+          <input type="radio" name="correctAnswer" value="${id}" required />
+          <input type="text" name="answersIds" value="${id}" hidden required />
+          <input
+            type="text"
+            name="answers"
+            value="${text}"
+            placeholder="Введите текст ответа"
+            required
+          />
+          <input class="deleteButton" type="button" value="Удалить" />
+        </div>`;
+}
+
+function getId() {
+  const now = Date.now();
+  const randomNumberA = Math.floor(Math.random() * 1e8);
+  const randomNumberB = Math.floor(Math.random() * 1e8);
+  const randomNumberC = Math.floor(Math.random() * 1e8);
+  const id = `${now.toString(36)}-${randomNumberA.toString(
+    36
+  )}-${randomNumberB.toString(36)}-${randomNumberC.toString(36)}`;
+  return id;
+}
