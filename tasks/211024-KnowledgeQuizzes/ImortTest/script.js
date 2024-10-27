6654da87e1ac809805096d7209454d0c26f2e33c
Askadhhh <127306364+Askadhhh@users.noreply.github.com>
2024-10-27 23:47:39 +0500

    feat #7

--- tasks/211024-KnowledgeQuizzes/ImortTest/script.js
+++ tasks/211024-KnowledgeQuizzes/ImortTest/script.js
@@ -1,1 +1,74 @@
-import { loadData, saveData } from "../assets/js/helper.js";
+import { loadData, saveData, getId } from "../assets/js/helper.js";
+const importJsonTestForm = document.querySelector("#importJsonTestForm");
+importJsonTestForm.addEventListener("submit", (event) => {
+  event.preventDefault();
+  const formData = new FormData(event.target);
+  const textAreaValue = formData.get("jsonTest");
+  try {
+    const test = JSON.parse(textAreaValue);
+    if (validateTest(test)) {
+      console.log("импорт успешен");
+      const finalTest = replaceAllIds(test);
+      document.body.innerHTML += `<pre><code>${JSON.stringify(
+        finalTest,
+        null,
+        2
+      )}</code></pre>`;
+      const tests = loadData("tests");
+      tests.push(finalTest);
+      saveData(tests, "tests");
+    } else {
+      throw new Error("формату jsona не соответсвует");
+    }
+    console.log(test);
+  } catch (error) {
+    alert(`Ошибка импорта: ${error.message}`);
+  }
+});
+function validateTest(test) {
+  return (
+    isNotEmptyString(test.id) &&
+    isNotEmptyString(test.title) &&
+    isNotEmptyArray(test.questions, 1) &&
+    test.questions.every((question) => {
+      return (
+        isNotEmptyString(question.id) &&
+        isNotEmptyArray(question.answers, 2) &&
+        isNotEmptyString(question.correctAnswer) &&
+        isNotEmptyString(question.title) &&
+        question.answers.every((answer) => {
+          return isNotEmptyString(answer.id) && isNotEmptyString(answer.text);
+        })
+      );
+    })
+  );
+}
+function isNotEmptyString(value) {
+  return typeof value === "string" && value.trim().length > 0;
+}
+function isNotEmptyArray(value, minLength) {
+  return Array.isArray(value) && value.length >= minLength;
+}
+function replaceAllIds(test) {
+  return {
+    ...test,
+    id: getId(),
+    questions: test.questions.map((question) => {
+      const newCorrectAnswerId = getId();
+
+      return {
+        ...question,
+        id: getId(),
+        answers: question.answers.map((answer) => {
+          const id =
+            answer.id === question.correctAnswer ? newCorrectAnswerId : getId();
+          return {
+            ...answer,
+            id,
+          };
+        }),
+        correctAnswer: newCorrectAnswerId,
+      };
+    }),
+  };
+}
--- tasks/211024-KnowledgeQuizzes/assets/js/helper.js
+++ tasks/211024-KnowledgeQuizzes/assets/js/helper.js
@@ -5,3 +5,13 @@ export const loadData = (name) => {
   const data = localStorage.getItem(name);
   return data ? JSON.parse(data) : []; // Если данных нет, возвращаем пустой массив
 };
+export const getId = () => {
+  const now = Date.now();
+  const randomNumberA = Math.floor(Math.random() * 1e8);
+  const randomNumberB = Math.floor(Math.random() * 1e8);
+  const randomNumberC = Math.floor(Math.random() * 1e8);
+  const id = `${now.toString(36)}-${randomNumberA.toString(
+    36
+  )}-${randomNumberB.toString(36)}-${randomNumberC.toString(36)}`;
+  return id;
+};
